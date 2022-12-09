bb5daae49dc2c83544278527946256e523447a73
Dominic Chang <dchang3419@hotmail.com>
2022-12-09 15:52:31 -0500

    Create day1.jl

---
+++ day1.jl
@@ -0,0 +1,11 @@
+
+########################
+# Part 1
+########################
+callist = eval(Meta.parse("["*replace(replace(input,"\n\n"=>"],["), "\n"=>",")*"]"))
+maximum(sum, callist)
+
+########################
+# Part 2
+########################
+sum(sum.(sort(collect(callist), by=sum,rev=true)[begin:begin+2]))
