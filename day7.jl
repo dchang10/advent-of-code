faca0e3ba3b8ef843ef3f669d21fa4c6c2e2db37
Dominic Chang <dchang3419@hotmail.com>
2022-12-21 23:32:23 -0500

    Update day7.jl

--- day7.jl
+++ day7.jl
@@ -6,7 +6,6 @@ mutable struct RootDir <: ADir
   children::Vector{ANode}
   RootDir(x,y) = x != "//" ? error("Root Directory must be called \\") : new(x,y) 
 end
-
 RootDir(x::Vector{ANode}) = RootDir("//" , x)
 
 mutable struct Dir <: ADir
@@ -14,6 +13,7 @@ mutable struct Dir <: ADir
   children::Vector{ANode}
   parent::ADir
 end
+zero(::Dir) = nothing
 
 mutable struct File <: ANode
   name::String
@@ -21,43 +21,19 @@ mutable struct File <: ANode
   parent::ADir
 end
 
-Base.show(io::IO, node::ANode) = print(io, "$(typeof(node) <: ADir ? [i.name for i in node.children] : typeof(node))")
-
-file = open((@__DIR__)*"/inputtest.txt")
-inputlist = [line for line in eachline(file)]
-close(file)
-
-currdir = RootDir(Vector{ANode}([]))
-
-line = split(popfirst!(inputlist), ' ')
+inputlist = [line for line in eachline(open((@__DIR__)*"/inputtest.txt"))][begin+1:end]
 line = split(popfirst!(inputlist), ' ')
 
 currdir = RootDir(Vector{ANode}([]))
-while length(inputlist) > -1
-    print(line)
-    print(" ")
-    println(currdir)
-    if line[1] == "\$"
-        if line[2] == "cd"
-            if line[3] == ".."
-                currdir = currdir.parent
-            else #cd into child
-                for i in currdir.children
-                    if i.name == line[3]
-                        currdir = i
-                        break
-                    end
-                end
-            end
-        end
-    else
-        if line[1] == "dir"
-            append!(currdir.children, [Dir(line[2], Vector{ANode}([]), currdir)])
-        else
-            append!(currdir.children, [File(line[2], parse(Int, line[1]), currdir)])
-        end
+  
+# Create directory and file tree
+for _ in 0:length(inputlist)
+    if line[1] == "\$" && line[2] == "cd"
+        currdir = (line[3] == "..") ? currdir.parent : filter(x->x.name==line[3], currdir.children)[1]#cd into child
+    elseif line[1] != "\$"
+        line[1] == "dir" ? append!(currdir.children, [Dir(line[2], Vector{ANode}([]), currdir)]) : append!(currdir.children, [File(line[2], parse(Int, line[1]), currdir)])
     end
-    line = split(popfirst!(inputlist), ' ')
+    line = length(inputlist) > 0 ? split(popfirst!(inputlist), ' ') : []
 end
 
 while currdir.name != "//"
@@ -68,32 +44,12 @@ end
 # Part 1
 ########################
 
-function getsizeofchildren(currdir) 
-  tot = 0
-  dirlist = []
-  for node in currdir.children
-    if typeof(node) == File
-      tot += node.filesize
-    else
-      append!(dirlist, [node])
-    end
-  end
-  for i in dirlist
-    tot += getsizeofchildren(i)
-  end
-  return tot
-end
+getsizeofdir(currdir) = sum(map(x->typeof(x) == File ?  x.filesize : getsizeofdir(x), currdir.children))
 
 function addtosum(topdir)
   dirlist = filter(x-> typeof(x) == Dir, topdir.children)
-  temp = getsizeofchildren(topdir)
-  tot = 0
-  tot += temp <= 100000 ? temp : 0
-  for i in dirlist
-    tot += addtosum(i)
-  end
-  
-  return tot
+  temp = getsizeofdir(topdir)
+  return (temp <= 100000 ? temp : 0) + (length(dirlist) > 0 ? sum(addtosum.(dirlist)) : 0)
 end
 
 addtosum(currdir)
@@ -102,24 +58,13 @@ addtosum(currdir)
 # Part 2
 ########################
 
-totlist = []
-function addtosum(topdir)
-  global totlist
-  dirlist = filter(x-> typeof(x) == Dir, topdir.children)
-  temp = getsizeofchildren(topdir)
-  tot = 0
-  tot += temp 
-  for i in dirlist
-    tot += addtosum(i)
-  end
-  
+function getsizelist(topdir, totlist)
+  getsizelist.(filter(x-> typeof(x) == Dir, topdir.children), Ref(totlist))
+  temp = getsizeofdir(topdir)
   append!(totlist, temp)
-  return tot
 end
 
+totlist = []
+getsizelist(currdir, totlist)
 spacetofree = 30000000 + totlist[end] - 70000000
-
-addtosum(currdir)
-sort!(totlist)
-totlist[findfirst(>=(spacetofree), totlist)]
-
+totlist[findfirst(>=(spacetofree), sort!(totlist))]
