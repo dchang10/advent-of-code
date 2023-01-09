495f32dfb631bb44ab8c5d71609975f4c934302e
Dominic Chang <dchang3419@hotmail.com>
2023-01-09 03:02:57 -0500

    Update day10.jl

--- day10.jl
+++ day10.jl
@@ -7,16 +7,18 @@ step = 1
 register = 1
 totsignal = 0
 
-for operation in inputlist
+function takestep1(step, register)
   signal = step == 20 ? 20*register : 0
   signal = (step-20)%40 == 0 ? step*register : 0
-  totsignal += signal
+  return signal
+end
+
+for operation in inputlist
+  totsignal += takestep1(step, register)
   step += 1
   
   if operation[1] == "addx"
-    signal = step == 20 ? 20*register : 0
-    signal = (step-20)%40 == 0 ? step*register : 0
-    totsignal += signal
+    totsignal += takestep1(step, register)
     step += 1
     register += parse(Int, operation[2])
   end
@@ -32,24 +34,23 @@ yourpart(x,n) = [[x[i:min(i+n-1,length(x))]] for i in 1:n:length(x)]
 step = 1      
 register = 1
 signal = ""
-
-for operation in inputlist
+      
+function takestep2(signal)
   if step == register || (step - 1) == register || (step - 2) == register
     signal = signal*"#"
   else
      signal = signal *"."
   end
-  println("$step $register")
+  return signal
+end
+
+for operation in inputlist
+  signal = takestep2(signal)
   step += 1
   step %= 40
   
   if operation[1] == "addx"
-    if step == register || (step - 1) == register || (step - 2) == register
-      signal = signal*"#"
-    else
-     signal = signal *"."
-    end
-  println("$step $register")
+    signal = takestep2(signal)
     step += 1
     step %= 40
     register += parse(Int, operation[2])
